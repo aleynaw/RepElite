759cffbd95ac032cd0eae8158d3dcc0573fc08bc
aleynawarner <aleyna013@gmail.com>
2022-03-25 12:27:25 -0700

    Phase 3 - added test for getSkillLevelInt in ProfileTest

--- src/test/model/ProfileTest.java
+++ src/test/model/ProfileTest.java
@@ -24,12 +24,15 @@ class ProfileTest {
     void testGetSkillLevel() {
         Profile testP1 = new Profile("JD", 25, 1);
         assertEquals("1 - Beginner", testP1.getSkillLevel());
+        assertEquals(1, testP1.getSkillLevelInt());
 
         Profile testP2 = new Profile("JD", 25, 2);
         assertEquals("2 - Intermediate", testP2.getSkillLevel());
+        assertEquals(2, testP2.getSkillLevelInt());
 
         Profile testP3 = new Profile("JD", 25, 3);
         assertEquals("3 - Advanced", testP3.getSkillLevel());
+        assertEquals(3, testP3.getSkillLevelInt());
     }
 
 
